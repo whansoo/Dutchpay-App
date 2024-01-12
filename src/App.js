85e2904eb179e348c275c54098efbdf49690d1b5
whansoo <ghkstn7523@naver.com>
2024-01-12 22:44:44 +0900

    Feat: [client] font style 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CreateGroup } from './components/CreateGroup';
 import { AddMembers } from './components/AddMembers';
 import { ExpenseMain } from './components/ExpenseMain';
+
+
+
 
 function App() {
   return (
--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const CreateGroup = () => {
+ 
+const S = styled.span`
+  font-family: 'Pretendard-Regular';
+`;
   return (
+    <>
     <div>CreateGroup</div>
+    <S>CreateGroup</S>
+    </>
   )
 }
